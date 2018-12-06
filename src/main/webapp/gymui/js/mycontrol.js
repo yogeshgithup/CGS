f9742e731b1d31cc5d6ccff98120e79893c63411
patelsneh <patelsneh833@gmail.com>
2018-12-06 12:11:34 +0530

    Achievement Jquery For Add more 6/12/2018

--- src/main/webapp/gymui/js/mycontrol.js
+++ src/main/webapp/gymui/js/mycontrol.js
@@ -10,10 +10,47 @@ function createButton(fid)
 }
 function createTextbox1(fid)
 {
-    var ctrl=$("<input/>").attr({type:'textarea',id:fid,placeholder:'Description'});
+    var ctrl=$("<input/>").attr({
+        type:'text',
+        id:fid,
+        placeholder:'Description',
+        class: 'form-control' 
+    });
     return ctrl;
 }
 function createDiv(fid)
+{
+    var ctrl=$("<div/>").attr({id:fid});
+    return ctrl;
+}
+
+
+function createButton2(fid)
+{
+    var ctrl=$("<input/>").attr({type:'button',id:fid,value:'remove'});
+    return ctrl;
+}
+function createTextarea2(fid)
+{
+    var ctrl=$("<input/>").attr({
+        type:'textarea',
+        id:fid,
+        placeholder:'Achievement Description',
+        class: 'form-control' 
+    });
+    return ctrl;
+}
+function createTextbox2(fid)
+{
+    var ctrl=$("<input/>").attr({
+        type:'text',
+        id:fid,
+        placeholder:'Achievement Title',
+        class: 'form-control' 
+    });
+    return ctrl;
+}
+function createDiv2(fid)
 {
     var ctrl=$("<div/>").attr({id:fid});
     return ctrl;
